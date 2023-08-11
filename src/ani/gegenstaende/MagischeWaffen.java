30ff9ad2fb1b7ff7a3271702d1b375de71a57a6d
Max <117784868+KaiserATV@users.noreply.github.com>
2023-08-11 18:41:01 +0200

    Delete src directory

--- src/ani/gegenstaende/MagischeWaffen.java
+++
@@ -1,8 +0,0 @@
-package ani.gegenstaende;
-
-abstract public class MagischeWaffen extends Gegenstand {
-	MagischeWaffen(){
-		super();
-	}
-	protected static String efBesch[] = {"none","destroySword","monsterStDown","staerkeUp","tameMonster"};
-}
