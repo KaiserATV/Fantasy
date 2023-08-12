462d1e709920fb38d97a69c7cbfec5cb7d15bbe2
FabioKn <fabio.knoess@t-online.de>
2023-08-12 17:39:31 +0200

    Magic Numbers entfernt

--- src/fabio/gui/karte/Karte.java
+++ src/fabio/gui/karte/Karte.java
@@ -1,22 +1,45 @@
 package fabio.gui.karte;
 
-import java.util.Arrays;
-import java.util.Random;
-import java.util.List;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Point;
 import java.util.ArrayList;
-import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import ani.fantasyLebewesen.Spieler;
 
-import java.awt.Image;
-import java.awt.Graphics;
-import java.awt.CardLayout;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.GridBagLayout;
-import java.awt.GridBagConstraints;
-
 public class Karte {
+
+        private static final int FELD_BREITE = 25;
+        private static final int FELD_HOEHE = FELD_BREITE;
+        private static final int KARTE_BREITE = 70;
+        private static final int KARTE_HOEHE = KARTE_BREITE;
+        private static final int GEBAEUDE_RATIO = 30;
+        private static final int MAX_VERBINDUNGEN_VON_ECKE = 40;
+        private static final String WEG_BILD_PATH = "Fantasy/src/img/Weg.png";
+        private static final String BAUM_BILD_PATH = "Fantasy/src/img/Laub.png";
+        private static final String GEBAEUDE_BILD_PATH = "Fantasy/src/img/Haus.png";
+        private FeldTyp[][] karte;
+        private JPanel contentPane = new JPanel();
+        private JFrame frame;
+        private Image wegBild;
+        private Image baumBild;
+        private Image gebaeudeBild; 
+
+        public boolean add(Spieler arg0) {
+                return spielerListe.add(arg0);
+        }
+
         public enum FeldTyp {
                 WEG,
                 BAUM,
@@ -26,23 +49,65 @@ public class Karte {
         }
       
         
-        private JPanel contentPane = new JPanel();
-        private FeldTyp[][] karte;
-        private JFrame frame;
-        private Image wegBild;
-        private Image baumBild;
-        private Image gebaeudeBild;
-        private static final int FELD_BREITE = 25;
-        private static final int FELD_HOEHE = 25;
+        /**
+         * Zeichnet die Karte und den Spieler.
+         * 
+         * @param g Das Graphics-Objekt, das zum Zeichnen verwendet wird.
+         */
+        public class KartenPanel extends JPanel {
+                
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                public Dimension getPreferredSize() {
+                        return new Dimension(karte.length * FELD_BREITE, karte[0].length * FELD_HOEHE);
+                }
+
+                @Override
+                protected void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+
+                        for (int i = 0; i < karte.length; i++) {
+                                for (int j = 0; j < karte[0].length; j++) {
+                                        Image aktuellesBild = null;
+                                        switch (karte[i][j]) {
+                                        case WEG:
+                                                aktuellesBild = wegBild;
+                                                break;
+                                        case BAUM:
+                                                aktuellesBild = baumBild;
+                                                break;
+                                        case Juwelier:
+                                        case Buchhandlung:
+                                        case Schmiede:
+                                                aktuellesBild = gebaeudeBild;
+                                                break;
+                                        }
+                                        g.drawImage(aktuellesBild, i * FELD_BREITE, j * FELD_HOEHE, FELD_BREITE, FELD_HOEHE, this);
+
+                                        // Hier zeichnest du die Spieler, falls sie auf dieser Kachel sind
+                                        for (Spieler spieler: spielerListe) {
+                                                if (spieler.getPosition().equals(new Point(i, j))) {
+                                                        spieler.draw(g, FELD_BREITE);
+                                                }
+                                        }
+                                }
+                        }
+                }
+        }
+              
         private List < Spieler > spielerListe = new ArrayList < > ();
-       
+
+        // Getter-Methode für das JFrame-Objekt der Karte.
+        // @return Gibt den Frame zurück, auf dem die Karte dargestellt wird.
+
         public Karte() {
-                karte = new FeldTyp[70][70];
+                karte = new FeldTyp[KARTE_BREITE][KARTE_HOEHE];
                 initKarte();
 
-                wegBild = new ImageIcon("Fantasy/src/img/Weg.png").getImage();
-                baumBild = new ImageIcon("Fantasy/src/img/Laub.png").getImage();
-                gebaeudeBild = new ImageIcon("/home/fknoess/FantasieGame2.0/Fantasy/src/img/Haus.png").getImage();
+                wegBild = new ImageIcon(WEG_BILD_PATH).getImage();
+                baumBild = new ImageIcon(BAUM_BILD_PATH).getImage();
+                gebaeudeBild = new ImageIcon(GEBAEUDE_BILD_PATH).getImage();
 
                 frame = new JFrame("Karte");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -58,8 +123,6 @@ public class Karte {
 
                 KartenPanel kartenPanel = new KartenPanel();
                 JScrollPane scrollPane = new JScrollPane(kartenPanel);
-
-                scrollPane.setPreferredSize(new Dimension(karte.length * FELD_BREITE, karte[0].length * FELD_HOEHE));
                 contentPane.add(scrollPane, gbc);
                 contentPane.setVisible(true);
                 frame.setLayout(new CardLayout());
@@ -73,22 +136,43 @@ public class Karte {
 
         }
 
-        // Getter-Methode für das JFrame-Objekt der Karte.
-        // @return Gibt den Frame zurück, auf dem die Karte dargestellt wird.
-
         public JFrame getFrame() {
                 return frame;
-        }
-
-        public void addSpieler(Spieler spieler) {
-                spielerListe.add(spieler);
         }
 
         // Getter-Methode für die Anzahl der Zellen in der Karte.
         // @return Gibt die Länge des karte-Arrays zurück.
 
+        public void addSpieler(Spieler spieler) {
+                spielerListe.add(spieler);
+        }
+
         public int getNUM_CELLS() {
                 return karte.length;
+        }
+
+        /**
+         * Getter-Methode, um den FeldTyp an einer gegebenen Position zu erhalten.
+         * 
+         * @param point Die Position auf der Karte.
+         * @return Gibt den FeldTyp an der gegebenen Position zurück.
+         */
+        public FeldTyp getFeldTypAtPosition(Point point) {
+                if (point.x >= 0 && point.x < karte.length && point.y >= 0 && point.y < karte[0].length) {
+                        return karte[point.x][point.y];
+                } else {
+                        throw new IndexOutOfBoundsException("Position außerhalb der Karte!");
+                }
+        }
+
+        // Hinzugefügt: Getter für das karte-Array
+        public FeldTyp[][] getKarte() {
+                return karte;
+        }
+
+        // Hinzugefügt: Setter für das karte-Array
+        public void setKarte(FeldTyp[][] karte) {
+                this.karte = karte;
         }
 
         /**
@@ -101,7 +185,7 @@ public class Karte {
                 }
 
                 Random rand = new Random();
-                int gebaeudeAnzahl = (karte.length * karte[0].length) / 30;
+                int gebaeudeAnzahl = (karte.length * karte[0].length) / GEBAEUDE_RATIO;
                 List < Point > gebaeudeListe = new ArrayList < > ();
 
                 for (int i = 0; i < gebaeudeAnzahl; i++) {
@@ -130,7 +214,7 @@ public class Karte {
 
                 for (Point ecke: ecken) {
                         List < Point > kopieGebaeudeListe = new ArrayList < > (gebaeudeListe);
-                        verbindeMitNaechstenGebaeuden(ecke, kopieGebaeudeListe, 40); // Verbinde bis zu 10 Gebäude von jeder Ecke
+                        verbindeMitNaechstenGebaeuden(ecke, kopieGebaeudeListe, MAX_VERBINDUNGEN_VON_ECKE); // Verbinde bis zu xx Gebäude von jeder Ecke
                         // aus
                 }
         }
@@ -210,80 +294,5 @@ public class Karte {
                                 karte[x][y] = FeldTyp.WEG;
                         }
                 }
-        }
-
-        /**
-         * Getter-Methode, um den FeldTyp an einer gegebenen Position zu erhalten.
-         * 
-         * @param point Die Position auf der Karte.
-         * @return Gibt den FeldTyp an der gegebenen Position zurück.
-         */
-        public FeldTyp getFeldTypAtPosition(Point point) {
-                if (point.x >= 0 && point.x < karte.length && point.y >= 0 && point.y < karte[0].length) {
-                        return karte[point.x][point.y];
-                } else {
-                        throw new IndexOutOfBoundsException("Position außerhalb der Karte!");
-                }
-        }
-
-        /**
-         * Zeichnet die Karte und den Spieler.
-         * 
-         * @param g Das Graphics-Objekt, das zum Zeichnen verwendet wird.
-         */
-        public class KartenPanel extends JPanel {
-                private static final long serialVersionUID = 1L;
-
-				public KartenPanel() {
-                        int padding = 100; // Ändern Sie den Wert entsprechend Ihrem Wunschabstand
-                        this.setBorder(BorderFactory.createEmptyBorder(padding, padding, padding, padding));
-                }
-
-                @Override
-                protected void paintComponent(Graphics g) {
-                        super.paintComponent(g);
-
-                        for (int i = 0; i < karte.length; i++) {
-                                for (int j = 0; j < karte[0].length; j++) {
-                                        Image aktuellesBild = null;
-                                        switch (karte[i][j]) {
-                                        case WEG:
-                                                aktuellesBild = wegBild;
-                                                break;
-                                        case BAUM:
-                                                aktuellesBild = baumBild;
-                                                break;
-                                        case Juwelier:
-                                        case Buchhandlung:
-                                        case Schmiede:
-                                                aktuellesBild = gebaeudeBild;
-                                                break;
-                                        }
-                                        g.drawImage(aktuellesBild, i * FELD_BREITE, j * FELD_HOEHE, FELD_BREITE, FELD_HOEHE, this);
-
-                                        // Hier zeichnest du die Spieler, falls sie auf dieser Kachel sind
-                                        for (Spieler spieler: spielerListe) {
-                                                if (spieler.getPosition().equals(new Point(i, j))) {
-                                                        spieler.draw(g, FELD_BREITE);
-                                                }
-                                        }
-                                }
-                        }
-                }
-
-                @Override
-                public Dimension getPreferredSize() {
-                        return new Dimension(karte.length * 25, karte[0].length * 25);
-                }
-        }
-
-        // Hinzugefügt: Getter für das karte-Array
-        public FeldTyp[][] getKarte() {
-                return karte;
-        }
-
-        // Hinzugefügt: Setter für das karte-Array
-        public void setKarte(FeldTyp[][] karte) {
-                this.karte = karte;
         }
 }
